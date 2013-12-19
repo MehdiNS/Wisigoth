65c9d964fcc015dba792e3841f9acb314d6b5b1e
David Levayer <david.levayer@gmail.com>
2013-12-19 13:28:47 +0100

    MaJ design MainMenuActivity

--- src/com/project/wisigoth/MainActivity.java
+++ src/com/project/wisigoth/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends Activity {
         //setContentView(R.layout.activity_main);
         Intent intent = new Intent(this, MainMenuActivity.class);
         startActivity(intent);
+        this.finish();
     }
 
 
--- src/com/project/wisigoth/MainMenuActivity.java
+++ src/com/project/wisigoth/MainMenuActivity.java
@@ -2,7 +2,9 @@ package com.project.wisigoth;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
 
 public class MainMenuActivity extends Activity {
 
@@ -17,6 +19,16 @@ public class MainMenuActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main_menu, menu);
 		return true;
+	}
+	
+	public void quitter(View view) {
+		this.finish();
+		
+	}
+	
+	public void onStop() {
+		Log.i("Wisigoth onStop","MainMenuActivity : passage dans onStop()");
+		super.onStop();
 	}
 
 }
