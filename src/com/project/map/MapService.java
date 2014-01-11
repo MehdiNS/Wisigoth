044eb269f93e29390ea64403f64a4848708c777f
David Levayer <david.levayer@gmail.com>
2014-01-11 19:11:24 +0100

    trigger si appli onPause() JUSTE pour Polytech

--- src/com/project/map/MapService.java
+++ src/com/project/map/MapService.java
@@ -16,11 +16,13 @@ import android.util.Log;
 import android.widget.Toast;
 
 public class MapService extends BroadcastReceiver {
-	private static final int NOTIFICATION_ID = 1000;
+	private static final int NOTIFICATION_ID = 1001;
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		Log.i("Wisigoth onStop","map");
+		Toast.makeText(context, "Welcome to my Area", 600).show();
+		
 		String key = LocationManager.KEY_PROXIMITY_ENTERING;
 		Boolean entering = intent.getBooleanExtra(key, false);
 		if (entering) {
@@ -33,7 +35,7 @@ public class MapService extends BroadcastReceiver {
 		Intent notificationIntent = new Intent(context, WebviewActivity.class);
 		PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, notificationIntent, 0);
 		Notification notification = createNotification();
-		notification.setLatestEventInfo(context, "Wisigoth Proximity Alert!", "You are near your point of interest.", pendingIntent);
+		notification.setLatestEventInfo(context, "Wisigoth", "Cliquer ici pour découvrir le point d'intérêt !", pendingIntent);
 
 		notificationManager.notify(NOTIFICATION_ID, notification);
 	}
